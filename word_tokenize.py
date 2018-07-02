d35afd2b2f50dcf46c23c59841df31f766dce2ab
Jeff Thomas <jeff.thomas137@gmail.com>
2018-07-01 21:33:12 -0400

    Cleaning data and plotting frequency of words in project request essay

--- word_tokenize.py
+++ word_tokenize.py
@@ -16,7 +16,7 @@ dictionary = set(word.lower() for passage in train for word in word_tokenize(pas
 
 # This is two for loops, with a dictionary created.
 # It compares all words in train, with all words in dictionary. Then it adds the sentiment.
-t = [({word: (word in word_tokenize(x[0])) for word in dictionary}, x[1]) for x in train]
+t = [({word: (word in word_tokenize(x[0])) for word in dictionary}, x[1]) for x in res]
 
 classifier = nltk.NaiveBayesClassifier.train(t)
 
