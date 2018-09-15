0fac1637e6bc131c51a7c6522e61d6c34fe9b87f
Jeff Thomas <jeff.thomas137@gmail.com>
2018-09-15 14:12:49 -0400

    Adding Results folder

--- example_random_forest.py
+++ example_random_forest.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from nltk import sent_tokenize, word_tokenize, ngrams
+from nltk.corpus import stopwords
 
 # boston = load_boston()
 def ngramize(texts, n):
@@ -21,16 +22,11 @@ def ngramize(texts, n):
 raw = pd.read_csv('/Users/jif/Donors_choose/report_all_projects.csv', encoding='utf-8', sep=';', iterator=True, chunksize=10000)
 raw_chunk_1 = raw.get_chunk(100)
 
+print(list(raw_chunk_1.columns.values))
+
 # Determine whether the project will get funded--
 # change project will make(subject/object cost/use), how desperately class needs it(geographical location (school state, morphology)/school district (school district)/description of students (percentage of students free lunch)),
 # gender, size of school (school district), number of projects submitted prior (transform of teacher first project posted date and count previous projects by teacher id), teacher prefix [Dr, Mr, Mrs, Ms, Teacher, N/A] (teacher prefix), teacher name (teacher id)
-
-
-# There are no scl_Y values
-# scl_X = school_chunk_1.values[:,:]
-
-# # There are no tchr_Y values
-# tchr_X = teacher_chunk_1.values[:,:]
 
 X = raw_chunk_1.values[:,3]
 y = raw_chunk_1.values[:,9]
@@ -49,8 +45,17 @@ for i in range(len(train)):
 dictionary = list(word.lower().encode("utf-8") for passage in train for word in word_tokenize(passage[0]))
 # dictionary = dictionary[:100]
 
+# Eliminate stop words
+stop_words = set(stopwords.words('english'))
+filtered_sentence = [w for w in dictionary if not w in stop_words]
+
+# Eliminate punctuation
+punct_words = set(['.',',','?','!','\"','\'',':',';','\\','&'])
+proj_specific_stop_words = set(['need','students'])
+final_sentence = [w for w in filtered_sentence if not w in punct_words and w not in proj_specific_stop_words]
+
 # Can use dictionary or filtered_sentence as a parameter here.
-freq_n1 = nltk.FreqDist(dictionary)
+freq_n1 = nltk.FreqDist(final_sentence)
 # print(freq_n1)
 # print(text)
 
@@ -62,3 +67,12 @@ freq_n1 = nltk.FreqDist(dictionary)
 #d3 = ngramize(text,n=3)
 #freq_n3 = nltk.FreqDist(d3)
 freq_n1.plot(50, cumulative=False, title='Most Common Words')
+
+# Predict things
+
+# This is two for loops.
+# It splits up all words, in the process of creating a set of all words in the text.
+# dictionary = set(word.lower() for passage in res for word in word_tokenize(passage[0]))
+# This is two for loops, with a dictionary created.
+# It compares all words in train, with all words in dictionary. Then it adds the sentiment.
+# t = [({word: (word in word_tokenize(x[0])) for word in dictionary}, x[1]) for x in train]
