ac7436c750af6567408ca6331a868011b0f87360
Jeff Thomas <jeff.thomas137@gmail.com>
2018-09-23 18:48:29 -0400

    Fixing error for rows without project need statements

--- example_random_forest.py
+++ example_random_forest.py
@@ -1,5 +1,6 @@
 # from sklearn.datasets import load_boston
 # from sklearn.ensemble import RandomForestRegressor
+import math
 import nltk
 import pandas as pd
 import numpy as np
@@ -20,7 +21,7 @@ def ngramize(texts, n):
 # school_chunk_1 = school_chunk_1.drop(['School Name','School Zip','School City','School County'], axis=1)
 
 raw = pd.read_csv('/Users/jif/Donors_choose/report_all_projects.csv', encoding='utf-8', sep=';', iterator=True, chunksize=10000)
-# raw_chunk_1 = raw.get_chunk(100)
+raw_chunk_1 = raw.get_chunk(100)
 
 df_empty = pd.DataFrame()
 for chunk in raw:
@@ -42,13 +43,21 @@ train = zip(doc_list,response_list)
 text = []
 
 for i in range(len(train)):
-    # This line is failing, with the following error (when all raw rows included):
-    # "TypeError: expected string or buffer"
-    text.append(word_tokenize(train[i][0]))
+    # print(i," ",train[i][0])
+    if train[i][0] == train[i][0]:
+        # print("list is empty")
+        text.append(word_tokenize(train[i][0]))
+
+dictionary = []
+for passage in train:
+    if passage[0] == passage[0]:
+        for word in word_tokenize(passage[0]):
+            # print(word.lower())
+            dictionary.append(word.lower().encode("utf-8"))
 
 # This is two for loops.
 # It splits up all words, in the process of creating a set of all words in the text.
-dictionary = list(word.lower().encode("utf-8") for passage in train for word in word_tokenize(passage[0]))
+# dictionary = list(word.lower().encode("utf-8") for passage in train for word in word_tokenize(passage[0]))
 # dictionary = dictionary[:100]
 
 # Eliminate stop words
@@ -61,18 +70,18 @@ proj_specific_stop_words = set(['need','students'])
 final_sentence = [w for w in filtered_sentence if not w in punct_words and w not in proj_specific_stop_words]
 
 # Can use dictionary or filtered_sentence as a parameter here.
-# freq_n1 = nltk.FreqDist(final_sentence)
-# # print(freq_n1)
-# # print(text)
-#
-# ######################################################
-# # Frequency distribution for bigrams, n-grams.
-# ######################################################
-# # This creates n-grams (n=3) for sentences that make up our textself.
-# # Each sentence is a description of why a donation is needed for a particular project.
-# #d3 = ngramize(text,n=3)
-# #freq_n3 = nltk.FreqDist(d3)
-# freq_n1.plot(50, cumulative=False, title='Most Common Words')
+freq_n1 = nltk.FreqDist(final_sentence)
+# print(freq_n1)
+# print(text)
+
+######################################################
+# Frequency distribution for bigrams, n-grams.
+######################################################
+# This creates n-grams (n=3) for sentences that make up our textself.
+# Each sentence is a description of why a donation is needed for a particular project.
+#d3 = ngramize(text,n=3)
+#freq_n3 = nltk.FreqDist(d3)
+freq_n1.plot(50, cumulative=False, title='Most Common Words')
 
 # Predict things
 
